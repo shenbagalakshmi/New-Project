60d13fcd07d76acd34c5a7e03e53fa48d96a517e
shenbagalakshmi <shenbasl@gmail.com>
2018-05-29 07:36:59 +0530

    Add files via upload

--- src/app/admin-dashboard/dashboard-details/dashboard-details.component.ts
+++ src/app/admin-dashboard/dashboard-details/dashboard-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {ScriptLoaderService} from './../../services/script-loader.service'
 @Component({
   selector: 'app-dashboard-details',
   templateUrl: './dashboard-details.component.html',
@@ -7,11 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _script: ScriptLoaderService) { }
 
   ngOnInit() {
     // document.getElementById('liveLocationDiv').style.display='none';
     // document.getElementById('teamLocationDiv').style.display='none';
   }
 
+  ngAfterViewInit() {
+    this._script.loadScripts('app-dashboard-details',
+        ['assets/js/dashboard.js','assets/js/charts/flot.bundle.js','assets/js/charts/flotcharts.js']);
+
+}
 }
